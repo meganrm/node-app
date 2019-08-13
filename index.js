2d9210446e8c88b9033def4a7cbf38fef462f700
meganrm <meganrm@gmail.com>
2019-08-13 16:35:54 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+const server = require('./lib/server.js');
+const express = require('express');
+const PORT = process.env.PORT || 9000;
+const app = express();
+const database = require('./post_to_indivisible/townhall-firebase-listener');
+
+app.get('*', function(request, response) {
+  console.log('New request:', request.url);
+  response.sendFile('index.html', { root: '.' });
+}); 
+
+server.start(app, PORT)
+  .then(console.log)
+  .then(database)
+  .catch((err) => console.log('request error', err));
